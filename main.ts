6ff141459cb9b46d3d1c4689e86adec737302468
Limbo8rr <77548425+Limbo8rr@users.noreply.github.com>
2021-08-06 15:31:20 -0700

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,12 +1,16 @@
 controller.menu.onEvent(ControllerButtonEvent.Pressed, function () {
     // `
     if (!(gamePaused)) {
-        sprites.setDataNumber(enemySprite, "prePauseVelocityX", enemySprite.vx)
-        sprites.setDataNumber(enemySprite, "vy", enemySprite.vy)
-        enemySprite.setVelocity(0, 0)
+        for (let value of sprites.allOfKind(SpriteKind.Enemy)) {
+            sprites.setDataNumber(value, "prePauseVelocityX", value.vx)
+            sprites.setDataNumber(value, "vy", value.vy)
+            value.setVelocity(0, 0)
+        }
         blockMenu.showMenu(["Black", "Yellow", "Close Menu"], MenuStyle.List, MenuLocation.TopRight)
     } else {
-        enemySprite.setVelocity(sprites.readDataNumber(enemySprite, "prePauseVelocityX"), sprites.readDataNumber(enemySprite, "vy"))
+        for (let value of sprites.allOfKind(SpriteKind.Enemy)) {
+            value.setVelocity(sprites.readDataNumber(value, "prePauseVelocityX"), sprites.readDataNumber(value, "vy"))
+        }
         blockMenu.closeMenu()
     }
     gamePaused = !(gamePaused)
@@ -17,33 +21,37 @@ blockMenu.onMenuOptionSelected(function (option, index) {
     } else if (option == "Yellow") {
         scene.setBackgroundColor(5)
     } else {
-        enemySprite.setVelocity(sprites.readDataNumber(enemySprite, "prePauseVelocityX"), sprites.readDataNumber(enemySprite, "vy"))
+        for (let value of sprites.allOfKind(SpriteKind.Enemy)) {
+            value.setVelocity(sprites.readDataNumber(value, "prePauseVelocityX"), sprites.readDataNumber(value, "vy"))
+        }
         blockMenu.closeMenu()
         gamePaused = !(gamePaused)
     }
 })
 let gamePaused = false
 let enemySprite: Sprite = null
-enemySprite = sprites.create(img`
-    . . . . c c c c c c . . . . . . 
-    . . . c 6 7 7 7 7 6 c . . . . . 
-    . . c 7 7 7 7 7 7 7 7 c . . . . 
-    . c 6 7 7 7 7 7 7 7 7 6 c . . . 
-    . c 7 c 6 6 6 6 c 7 7 7 c . . . 
-    . f 7 6 f 6 6 f 6 7 7 7 f . . . 
-    . f 7 7 7 7 7 7 7 7 7 7 f . . . 
-    . . f 7 7 7 7 6 c 7 7 6 f c . . 
-    . . . f c c c c 7 7 6 f 7 7 c . 
-    . . c 7 2 7 7 7 6 c f 7 7 7 7 c 
-    . c 7 7 2 7 7 c f c 6 7 7 6 c c 
-    c 1 1 1 1 7 6 f c c 6 6 6 c . . 
-    f 1 1 1 1 1 6 6 c 6 6 6 6 f . . 
-    f 6 1 1 1 1 1 6 6 6 6 6 c f . . 
-    . f 6 1 1 1 1 1 1 6 6 6 f . . . 
-    . . c c c c c c c c c f . . . . 
-    `, SpriteKind.Enemy)
-enemySprite.setStayInScreen(true)
-enemySprite.setBounceOnWall(true)
-enemySprite.setVelocity(randint(0, 75), randint(0, 75))
-gamePaused = false
-blockMenu.setColors(15, 15)
+for (let index = 0; index < 4; index++) {
+    enemySprite = sprites.create(img`
+        . . . . c c c c c c . . . . . . 
+        . . . c 6 7 7 7 7 6 c . . . . . 
+        . . c 7 7 7 7 7 7 7 7 c . . . . 
+        . c 6 7 7 7 7 7 7 7 7 6 c . . . 
+        . c 7 c 6 6 6 6 c 7 7 7 c . . . 
+        . f 7 6 f 6 6 f 6 7 7 7 f . . . 
+        . f 7 7 7 7 7 7 7 7 7 7 f . . . 
+        . . f 7 7 7 7 6 c 7 7 6 f c . . 
+        . . . f c c c c 7 7 6 f 7 7 c . 
+        . . c 7 2 7 7 7 6 c f 7 7 7 7 c 
+        . c 7 7 2 7 7 c f c 6 7 7 6 c c 
+        c 1 1 1 1 7 6 f c c 6 6 6 c . . 
+        f 1 1 1 1 1 6 6 c 6 6 6 6 f . . 
+        f 6 1 1 1 1 1 6 6 6 6 6 c f . . 
+        . f 6 1 1 1 1 1 1 6 6 6 f . . . 
+        . . c c c c c c c c c f . . . . 
+        `, SpriteKind.Enemy)
+    enemySprite.setStayInScreen(true)
+    enemySprite.setBounceOnWall(true)
+    enemySprite.setVelocity(randint(0, 75), randint(0, 75))
+    gamePaused = false
+    blockMenu.setColors(15, 5)
+}
